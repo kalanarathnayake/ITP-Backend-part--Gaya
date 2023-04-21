50f14032bb2b7e5add398ade37546631fc6f7462
kalanarathnayake <kalanagayanga8@gmail.com>
2023-04-21 13:30:11 +0530

    fix: minor fix in model

--- model/Meeting.model.js
+++ model/Meeting.model.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const meetingSchema = new Schema({
-    participants: { type: String, required: true },
+    participants: { type: [String], required: true },
     platform: { type: String, required: true },
     date: { type: Date, required: true },
     time: { type: String, required: true },
